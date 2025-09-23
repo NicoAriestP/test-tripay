3ac88bef48202c66fdba6abe0f92ccb3c8a90036
NicoAriestP <nicoariestputra@gmail.com>
2025-09-23 14:22:52 +0700

    wip: store invoice

--- app/Models/Invoice.php
+++ app/Models/Invoice.php
@@ -9,4 +9,24 @@ class Invoice extends Model
 {
     /** @use HasFactory<\Database\Factories\InvoiceFactory> */
     use HasFactory;
+
+    protected $fillable = [
+        'product_id',
+        'tripay_reference',
+        'buyer_email',
+        'buyer_phone',
+        'raw_response',
+    ];
+
+    protected $casts = [
+        'raw_response' => 'array',
+        'created_at' => 'datetime',
+        'updated_at' => 'datetime',
+        'deleted_at' => 'datetime',
+    ];
+
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
+    }
 }
