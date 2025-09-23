8a7d0b1dacc65d2c85f9d8dd1e24845ec44e6841
NicoAriestP <nicoariestputra@gmail.com>
2025-09-23 14:22:30 +0700

    wip: category (pending)

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -9,4 +9,19 @@ class Category extends Model
 {
     /** @use HasFactory<\Database\Factories\CategoryFactory> */
     use HasFactory;
+
+    protected $fillable = [
+        'name',
+    ];
+
+    protected $casts = [
+        'created_at' => 'datetime',
+        'updated_at' => 'datetime',
+        'deleted_at' => 'datetime',
+    ];
+
+    public function products()
+    {
+        return $this->hasMany(Product::class);
+    }
 }
