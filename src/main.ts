abb5dd53abb7dddb7dd6f3ef95c5c2764ffa5bb2
NicoAriestP <nicoariestputra@gmail.com>
2025-09-24 16:12:51 +0700

    feat: integrate PrimeVue with Aura theme configuration

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,27 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import PrimeVue from 'primevue/config'
+import Aura from '@primeuix/themes/aura'
 
 import App from './App.vue'
 import router from './router'
+
+import './assets/css/main.css'
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(PrimeVue, {
+  // Default theme configuration
+  theme: {
+    preset: Aura,
+    options: {
+      prefix: 'p',
+      darkModeSelector: 'system',
+      cssLayer: false,
+    },
+  },
+})
 
 app.mount('#app')
