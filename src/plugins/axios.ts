2dcfc16cf39ce55d66294f2e2d824ea3dd9f6690
NicoAriestP <nicoariestputra@gmail.com>
2025-09-25 21:41:08 +0700

    fix: update axios base URL to use VITE_BASE_API_URL

--- src/plugins/axios.ts
+++ src/plugins/axios.ts
@@ -1,4 +1,4 @@
 import axios from 'axios'
 
-axios.defaults.baseURL = import.meta.env.VITE_API_URL
+axios.defaults.baseURL = import.meta.env.VITE_BASE_API_URL
 axios.defaults.headers.common['Content-Type'] = 'application/json'
