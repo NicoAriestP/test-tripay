0b3643d512f20d1aaf3195b32ecf5ebf6077672d
NicoAriestP <nicoariestputra@gmail.com>
2025-09-25 21:43:47 +0700

    update product api functions to use ProductFormData type and add transactions apis

--- src/apis/index.ts
+++ src/apis/index.ts
@@ -1,17 +1,18 @@
 import axios from 'axios'
+import type { ProductFormData } from '@/types/ProductType'
 
-export default function useProducts() {
+export function useProducts() {
   const API_PATH = '/api/products'
 
   async function listProducts() {
     return await axios.get(API_PATH, { params: { search: '' } })
   }
 
-  async function createProduct(data: any) {
+  async function createProduct(data: ProductFormData) {
     return await axios.post(API_PATH, data)
   }
 
-  async function updateProduct(id: number, data: any) {
+  async function updateProduct(id: number, data: ProductFormData) {
     return await axios.put(`${API_PATH}/${id}`, data)
   }
 
@@ -24,5 +25,22 @@ export default function useProducts() {
     createProduct,
     updateProduct,
     deleteProduct,
+  }
+}
+
+export function useTransactions() {
+  const API_PATH = '/api/transactions'
+
+  async function listPaymentChannels() {
+    return await axios.get(`${API_PATH}/payment-channels`)
+  }
+
+  async function createTransaction(product_id: number, channel_code: string) {
+    return await axios.post(`${API_PATH}/create`, { product_id, channel_code })
+  }
+
+  return {
+    listPaymentChannels,
+    createTransaction,
   }
 }
