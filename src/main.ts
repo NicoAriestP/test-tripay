fd6a10c149cc45a5698defc02966a9ddb28885a4
NicoAriestP <nicoariestputra@gmail.com>
2025-09-25 23:37:56 +0700

    feat: integrate PrimeVue services and directives in main application setup

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,16 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import PrimeVue from 'primevue/config'
+import ConfirmationService from 'primevue/confirmationservice'
 import Aura from '@primeuix/themes/aura'
 
 import App from './App.vue'
 import router from './router'
+import ToastService from 'primevue/toastservice'
+import Tooltip from 'primevue/tooltip'
 
 import './assets/css/main.css'
+import './plugins/axios'
 
 const app = createApp(App)
 
@@ -23,5 +27,8 @@ app.use(PrimeVue, {
     },
   },
 })
+app.use(ConfirmationService)
+app.use(ToastService)
+app.directive('tooltip', Tooltip)
 
 app.mount('#app')
